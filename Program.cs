29f8e88adb9c38b34e51fd86d91a79b5e2e7c537
Adam Krček <adam.krcek@avast.com>
2022-08-31 19:32:24 +0200

    added dev cors

--- Program.cs
+++ Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+var devCorsPolicy = "devCorsPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(devCorsPolicy, builder => {
+        //builder.WithOrigins("http://localhost:800").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        //builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
+        //builder.SetIsOriginAllowed(origin => true);
+    });
+});
 builder.WebHost.UseUrls("http://0.0.0.0:5337");
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseCors(devCorsPolicy);
+app.MapControllers();
 app.Run();
